8fece31c622abb7e7015161f831dba8197c899af
Hawaichoti <40481848+Hawaichoti@users.noreply.github.com>
2020-04-03 23:22:04 +0530

    Update Person.java

--- src/com/w3epic/wiprotraining/assignment3/Person.java
+++ src/com/w3epic/wiprotraining/assignment3/Person.java
@@ -2,4 +2,20 @@ package com.w3epic.wiprotraining.assignment3;
 
 public class Person {
 	private String name;
-}
+	private String dateofBirth;
+	public Person(String name, String dateofBirth) {
+		this.name = name;
+		this.dateofBirth = dateofBirth;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public String getDob() {
+		return dateofBirth;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + "dateofBirth"+dateofBirth+ "]";
+	}
